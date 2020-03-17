33b3e97849958ba330feeca77401c83e7366cb91
singular-steve <singular1.steve@gmail.com>
2020-03-18 00:08:32 +0900

    add style sample code

---
+++ styletest/src/components/Child1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    {{msg}}
+    <child11/>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  import Child11 from "./Child11";
+  export default {
+    name: "Child1",
+    components: {Child11},
+    data() {
+      return {
+        msg: 'Child1'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .main {
+    border: solid 1px black;
+    background-color: yellow;
+  }
+  .test {
+    padding: 10px;
+    text-decoration: underline;
+    border: solid 1px black;
+  }
+</style>
---
+++ styletest/src/components/Child11.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <h3>Child - Child</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Child11"
+  }
+</script>
+
+<style scoped>
+  .test {
+    font-style: italic;
+  }
+</style>
