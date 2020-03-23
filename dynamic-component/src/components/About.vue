edde020b03693b2a26d07256027c7fda750f66ce
singular-steve <singular1.steve@gmail.com>
2020-03-23 23:20:08 +0900

    add sample code for recursive component

--- dynamic-component/src/components/About.vue
+++ dynamic-component/src/components/About.vue
@@ -2,15 +2,51 @@
   <div>
     <h1>About</h1>
     <h3>{{ new Date().toTimeString() }}</h3>
+    <h4>조직도</h4>
+    <tree v-bind:subs="orgcharts"></tree>
   </div>
 </template>
 
 <script>
+  import Tree from "./Tree";
+
   export default {
-    name: "about"
+    name: "about",
+    components: {Tree},
+    data: function() {
+      return {
+        orgcharts: [
+          {
+            name: 'Comi Company', type: 'company',
+            subs: [
+              { name: '1 Division', type: 'division',
+                subs: [
+                  { name: '1-1 Team', type: 'team'},
+                  { name: '1-2 Team', type: 'team'},
+                ]
+              },
+              { name: '2 Division', type: 'division',
+                subs: [
+                  { name: '2-1 Team', type: 'team'},
+                  { name: '2-2 Team', type: 'team'},
+                ]
+              },
+            ],
+          },
+        ]
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  li.company {
+    color: blue;
+  }
+  li.division {
+    color: steelblue;
+  }
+  li.team {
+    color: tomato;
+  }
 </style>
---
+++ dynamic-component/src/components/Tree.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="s in subs" v-bind:class="s.type">
+      {{s.name}}
+      <tree v-bind:subs="s.subs"></tree>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "Tree",
+    props: ['subs']
+  }
+</script>
